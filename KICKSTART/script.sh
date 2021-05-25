ff300a86fe600f59c57e66fccf870e66b2327fc7
nnleaf <nn.hoang.nguyen@gmail.com>
2021-05-25 09:29:15 -0400

    Checked with Rick
    Archiving - 20210520

--- KICKSTART/script.sh
+++ KICKSTART/script.sh
@@ -8,7 +8,8 @@ PASSWORD="user1234"
 
 #Create User Account
 /usr/sbin/useradd -m $USERNAME
-/usr/bin/echo "$USERNAME:$PASSWORD" | /usr/sbin/chpasswd
+passwd -d $USERNAME
+#/usr/bin/echo "$USERNAME:$PASSWORD" | /usr/sbin/chpasswd
 
 #Enable ethernet
 sed -i '/ONBOOT="no"/d' /etc/sysconfig/network-scripts/ifcfg-e*
@@ -27,13 +28,9 @@ echo "exec /usr/bin/xfce4-session" >> ~/.xinitrc
 rm -f /usr/share/xsessions/openbox.desktop
 
 #Install Packages
-wget -O /tmp/ks/google-chrome.rpm https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
-wget -O /tmp/ks/teams.rpm https://packages.microsoft.com/yumrepos/ms-teams/teams-1.3.00.25560-1.x86_64.rpm
 wget -O /tmp/ks/forticlient.rpm https://links.fortinet.com/forticlient/rhel/vpnagent
-yum -y install /tmp/ks/teams.rpm
-yum -y install /tmp/ks/google-chrome.rpm
 yum -y install /tmp/ks/forticlient.rpm
-yum -y install remmina gnome-system-monitor
+yum -y install remmina gnome-system-monitor pulseaudio-utils
 
 #Restore xfce4 panels
 rm -rf /root/.config/xfce4/
@@ -42,20 +39,26 @@ mkdir -p /root/.config/
 mkdir -p /home/"$USERNAME"/.config/
 cp -r /tmp/ks/xfce4/ /root/.config/.
 cp -r /tmp/ks/xfce4/ /home/"$USERNAME"/.config/.
-#Give ownership to created userNAME
-chown -R "$USERNAME":"$USERNAME" /home/"$USERNAME"/
-
+#Add *.desktop files
+cp -r /tmp/ks/autostart/ /home/"$USERNAME"/.config/.
+#Transfer Gnome Keyring
+cp -r /tmp/ks/keyrings/ /home/"$USERNAME"/.local/share/.
+#Transfer Remmina Template
+cp -r /tmp/ks/remmina /home/"$USERNAME"/.local/share/.
 #Set Wallpaper
 mkdir -p /usr/share/backgrounds/images/
 cp -r /tmp/ks/default.png /usr/share/backgrounds/images/default.png
+#Kiosk Mode
+cp -r /home/"$USERNAME"/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml /etc/xdg/xfce4/xfconf/xfce-perchannel-xml/
+sed -i 's/<channel name="xfce4-panel" version="1.0">/<channel name="xfce4-panel" version="1.0" locked="*" unlocked="root">/g' /etc/xdg/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml
+
+
+
+#Set ownership to created user's folder
+chown -R "$USERNAME":"$USERNAME" /home/"$USERNAME"/
 
 #Update CentOS
 yum -y update
-
-#Transfer Gnome Keyring
-mkdir -p /home/"$USERNAME"/.local/share/keyrings/
-cp -r /tmp/ks/keyrings/ /home/"$USERNAME"/.local/share/.
-chown -R "$USERNAME":"$USERNAME" /home/"$USERNAME"/.local/share/keyrings/
 
 #Cleanup
 rm -rf /tmp/ks/
