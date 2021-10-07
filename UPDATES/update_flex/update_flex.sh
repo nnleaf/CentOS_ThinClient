f5d952184de55bc2697edcd9e5bf9c3333f47acb
nnleaf <nn.hoang.nguyen@gmail.com>
2021-10-07 02:51:40 -0400

    Tested working on VM

--- UPDATES/update_flex/update_flex.sh
+++ UPDATES/update_flex/update_flex.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 #Nam - 20210813
 #Update Script v.1.1 > Teams/Zoom Build
+#
+# INSTRUCTIONS
+#
+# 1. Transfer to /tmp/
+# 2. SSH into client and run 
+#    sudo sh /tmp/update_flex/update_flex.sh
 
+#Variables
 username1="Agent"
 
 echo "====================================================="
@@ -14,28 +21,29 @@ echo "====================================================="
 #Temp folder
 sudo mkdir /tmp/update/
 
+#Install Teams
 sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 sudo flatpak install -y flathub com.microsoft.Teams
 sudo flatpak override com.microsoft.Teams --nofilesystem==/
-
 #Install Zoom
 wget -P /tmp/update/ https://zoom.us/client/latest/zoom_x86_64.rpm
 sudo yum -y localinstall /tmp/update/zoom_x86_64.rpm
 
-#Restore xfce4 Panels
-sed -i 's/<channel name="xfce4-panel" version="1.0" locked="*" unlocked="tmp">/<channel name="xfce4-panel" version="1.0">/g' /etc/xdg/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml
-
+#Add new xfce4 panels
+sudo sed -i 's/<channel name="xfce4-panel" version="1.0" locked="\*" unlocked="tmp">/<channel name="xfce4-panel" version="1.0">/g' /etc/xdg/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml
 sudo rm -rf /root/.config/xfce4/
 sudo rm -rf /home/"$username1"/.config/xfce4/
 sudo mkdir -p /root/.config/
 sudo mkdir -p /home/"$username1"/.config/
 sudo cp -r /tmp/update_flex/xfce4/ /root/.config/.
 sudo cp -r /tmp/update_flex/xfce4/ /home/"$username1"/.config/.
-sed -i 's/<channel name="xfce4-panel" version="1.0">/<channel name="xfce4-panel" version="1.0" locked="*" unlocked="tmp">/g' /etc/xdg/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml
-
+chown -R "$username1":"$username1" /home/"$username1"/
+#Kill Session to reload xfce4
+cp -r /home/"$username1"/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml /etc/xdg/xfce4/xfconf/xfce-perchannel-xml/
+sudo sed -i 's/<channel name="xfce4-panel" version="1.0">/<channel name="xfce4-panel" version="1.0" locked="*" unlocked="tmp">/g' /etc/xdg/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml
 #Set Permissions
 chown -R "$username1":"$username1" /home/"$username1"/
+sudo pkill -KILL -u Agent
 
 #Cleanup
 sudo rm -r /tmp/update/ /tmp/update_flex
-sudo reboot
