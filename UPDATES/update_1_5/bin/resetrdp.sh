3e02f738af11c6ebda02ef6a9432ab3506983840
nnleaf <nn.hoang.nguyen@gmail.com>
2022-03-21 14:26:38 -0400

    bug fix

--- UPDATES/update_1_5/bin/resetrdp.sh
+++ UPDATES/update_1_5/bin/resetrdp.sh
@@ -3,6 +3,6 @@
 #Resets remmina configuration on reboot
 username1="Agent"
 
-rm -rf /home/"$usernam1"/.local/share/remmina/
+rm -rf /home/"$username1"/.local/share/remmina/
 cp -r /root/remmina/ /home/"$username1"/.local/share/.
 chown "$username1":"$username1" -R /home/"$username1"/.local/share/remmina
